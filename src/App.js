b91bb50238c5ddeb64ebe95164725cc5cef882f5
leslie.wen <xsw1024@yeah.net>
2018-03-29 23:58:35 +0800

    change title

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
         <p className="App-intro">
           办理东航金卡， 有意者请联系 tel: 1 5 9 0 0 0 0 1 2 4 6
         </p>
